31719bc45c25ca0179a172a70d2df6803b2af619
Amir Fahd A. Hadji Usop <12872945+arvicxyz@users.noreply.github.com>
2024-03-02 12:30:38 +0800

    Create project

---
+++ MauiNoteTakingApp/MauiNoteTakingApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiNoteTakingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
